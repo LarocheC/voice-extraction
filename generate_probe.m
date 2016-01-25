8a3273ff89adb3a48863495a05ef93482625dc21
Clément L <clement.laroche@gmail.com>
2016-01-25 14:07:38 +0100

    White noise centered in 0

--- generate_probe.m
+++ generate_probe.m
@@ -1,4 +1,4 @@
-function probe = generate_probe(fs)
+function [probe, noise] = generate_probe(fs)
 if nargin<1
     fs = 4000;
 end
@@ -13,10 +13,12 @@ f3 = 262;
 harmonic_1 = 0.5 * (1 - heaviside(t-3)) .* generate_harmonics(f1, t, 3);
 harmonic_2 = 0.5 * heaviside(t-2) .* generate_harmonics(f3, t, 3); 
 
-noise_1 = 2.0 * (heaviside(t-1) - heaviside(t-1.1)) .* rand(1, length(t));
-noise_2 = 2.0 * (heaviside(t-2) - heaviside(t-2.1)) .* rand(1, length(t));
-noise_3 = 0.5 * (heaviside(t-3) - heaviside(t-3.3)) .* rand(1, length(t));
-noise_4 = 0.5 * (heaviside(t-4) - heaviside(t-4.3)) .* rand(1, length(t));
+noise_1 = 2.0 * (heaviside(t-1) - heaviside(t-1.1)) .* (rand(1, length(t))-0.5);
+noise_2 = 2.0 * (heaviside(t-2) - heaviside(t-2.1)) .* (rand(1, length(t))-0.5);
+noise_3 = 0.5 * (heaviside(t-3) - heaviside(t-3.3)) .* (rand(1, length(t))-0.5);
+noise_4 = 0.5 * (heaviside(t-4) - heaviside(t-4.3)) .* (rand(1, length(t))-0.5);
+
+noise = noise_1 + noise_2 + noise_3 + noise_4;
 
 harmochirp =       sin(2*pi*1*f1*(1+0.1*sin(2*t)) .* t) + ...
              0.3 * sin(2*pi*2*f1*(1+0.1*sin(2*t)) .* t) + ...

2dc5211e7126ea96b2dd4a83a9a2ce3174cf4a3e
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-01-25 16:32:13 +0100

    update sandbox

--- sandbox.m
+++ sandbox.m
@@ -1,6 +1,3 @@
-close all
-
-
 %% Generate probe signal
 [probe, noise] = generate_probe(6554);
 x = probe(1:32768).';
@@ -8,8 +5,9 @@ noise = noise(1:32768).';
 
 %% Setup filter banks
 clear opts;
-log2_oversampling = 6;
-oversampling = pow2(log2_oversampling);
+phi_log2_oversampling = 6;
+phi_oversampling = pow2(phi_log2_oversampling);
+nmf_log2_oversampling = 6;
 
 opts{1}.time.T = 8192;
 opts{1}.time.nFilters_per_octave = 8;
@@ -22,24 +20,24 @@ opts{2}.time.nFilters_per_octave = 2;
 opts{2}.time.cutoff_in_dB = 2;
 opts{2}.time.max_scale = length(x);
 opts{2}.time.is_phi_gaussian = true;
-opts{2}.time.S_log2_oversampling = log2_oversampling;
+opts{2}.time.S_log2_oversampling = phi_log2_oversampling;
 archs = sc_setup(opts);
 
-% Compute scattering transform
+%% Compute scattering transform
 [S, U, Y] = sc_propagate(x, archs);
 
 
-%% Generate references
+% Generate references
 spatial_subscripts = 1;
 refs_1 = generate_refs(S{1+1}.data, spatial_subscripts, S{1+1}.ranges{1+0});
 refs_2 = generate_refs(S{1+2}.data, spatial_subscripts, S{1+2}.ranges{1+0});
 
-%% Convert scattering nodes to matrices
+% Convert scattering nodes to matrices
 nFrames = size(S{1+1}.data, 1);
 nRefs_1 = size(refs_1, 2);
 Smat_1 = zeros(nRefs_1, nFrames);
 for ref1 = 1:nRefs_1
-    Smat_1(ref1, :) = subsref(S{1+1}.data, refs_1(1:oversampling:end, ref1));
+    Smat_1(ref1, :) = subsref(S{1+1}.data, refs_1(ref1));
 end
 nRefs_2 = size(refs_2, 2);
 Smat_2 = zeros(nRefs_2, nFrames);
@@ -47,14 +45,26 @@ for ref2 = 1:nRefs_2
     Smat_2(ref2, :) = subsref(S{1+2}.data, refs_2(:, ref2));
 end
 
+% Update the toolbox behavior so that it adapts to resampled inputs
+archs{2}.banks{1}.behavior.S.log2_oversampling = nmf_log2_oversampling;
+archs{2}.invariants{1}.behavior.S.log2_oversampling = nmf_log2_oversampling;
+S{1+1}.ranges{1+0}(2,1) = S{1+1}.ranges{1+0}(2,1) * downsampling;
+
+% Downsample manually
+nmf_oversampling = pow2(nmf_log2_oversampling);
+downsampling = phi_oversampling / nmf_oversampling;
+Smat_1 = Smat_1(:, 1:downsampling:end);
+Smat_2 = Smat_2(:, 1:downsampling:end);
+
+% Ensure non-negativity
 Smat_1(Smat_1<0)=0;
 Smat_2(Smat_2<0)=0;
 
 subplot(121); imagesc((Smat_1));
 subplot(122); imagesc((Smat_2));
+set(gcf, 'WindowStyle', 'docked');
 
-
-%% Create parameters
+% Create parameters
 [F1,T1]=size(Smat_1);
 [F2,T2]=size(Smat_2);
 
@@ -70,18 +80,15 @@ X = Smat_1;
 
 [ W, H2, e] = SPNMF_KL_W2TRAIN( X , W , W2, H2 , max_iter);
 
+subplot(111);
 plot(e);
 
-figure
-subplot(131); imagesc((W*W'*Smat_1));
-subplot(132); imagesc((W2*H2));
-subplot(133); imagesc((W*W'*Smat_1+W2*H2));
-
 %%
+
 Smat1_perc = W2 * H2;
 S1_perc = S{1+1};
 S1_perc.data = Smat1_perc.';
-Y2_perc = dS_backto_dY(S1_perc, archs{1});
+Y2_perc = dS_backto_dY(S1_perc, archs{2});
 Y2_perc = copy_metadata(Y{1+1}{1}, Y2_perc);
 U1_perc = dY_backto_dU(Y2_perc);
 Y1_perc = Y{1+0}{end};
@@ -98,5 +105,9 @@ x_perc = real(Y0_perc.data);
 
 plot(x_perc - noise);
 
+%%
+subplot(131); imagesc((W*W'*Smat_1));
+subplot(132); imagesc((W2*H2));
+subplot(133); imagesc((W*W'*Smat_1+W2*H2));
 
 

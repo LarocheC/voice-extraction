8118beb60fea3ec6077ee8dc6d5d7b48e521dc04
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-03-29 18:01:11 +0200

    segment hits

--- dictionary/parse_hits.m
+++ dictionary/parse_hits.m
@@ -14,19 +14,47 @@ hit_file_names = ...
 nHit_files = length(hit_file_names);
 
 nfft = 1024;
+hit_length = 131072;
+hits = cell(1, nHit_files);
+
 for hit_file_index = 1:nHit_files
     hit_file_name = hit_file_names{hit_file_index};
-    hit_file_path = fullfile(mix_path, hit_file_name)
+    hit_file_path = fullfile(mix_path, hit_file_name);
     x_indices = findstr(hit_file_path, 'x');
     last_x_index = x_indices(end);
     nHits = str2num(hit_file_path((last_x_index+1):(end-4)));
     [stereo_waveform, sample_rate] = audioread(hit_file_path);
     mono_waveform = mean(stereo_waveform, 2);
     odf = onset_detection_function(mono_waveform, sample_rate, nfft);
-    [peak_values, peak_locations] = ...
+    [peak_values, hit_locations] = ...
         findpeaks(odf, ...
         'NPeaks', nHits, ...
         'sort', 'descend', ...
         'MinPeakDistance', 44100 / nfft);
-    peak_locations = peak_locations * nfft;
+    hit_locations = hit_locations * nfft;
+    hit_locations = sort(hit_locations, 'ascend');
+    hits{hit_file_index} = cell(1, nHits);
+    for hit_index = 1:nHits
+        hit_location = hit_locations(hit_index);
+        hit_start = hit_location - hit_length / 8;
+        if hit_index > 1
+            hit_start = max(hit_start, hit_locations(hit_index-1));
+        else
+            hit_start = max(hit_start, 1);
+        end
+        hit_stop = hit_start + hit_length - 1;
+        if hit_index < nHits
+            hit_stop = min(hit_stop, hit_locations(hit_index+1) - hit_length/4);
+        else
+            hit_stop = min(hit_stop, length(mono_waveform));
+        end
+        hit_waveform = mono_waveform(hit_start:hit_stop);
+        if length(hit_waveform) < hit_length
+            hit_waveform = cat(1, hit_waveform, ...
+                zeros(hit_length - length(hit_waveform), 1));
+        end
+        hits{hit_file_index}{hit_index} = hit_waveform;
+    end
 end
+
+hits = [hits{:}];

30244897857d38fbdc02394db29368ff194c5f5c
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-05-04 13:36:30 +0200

    update manual_check

--- dictionary/manual_check.m
+++ dictionary/manual_check.m
@@ -1,8 +1,6 @@
 i = i+1;
 plot(hits{i}); sound(hits{i}, sample_rate);
 
-%% The following indices should be discarded:
-% 140
-% 142
-% 144
-% 420
+%% The following indices should be discarded: 140, 142, 144, 420
+discarded_indices = [140, 142, 144, 420];
+hits(discarded_indices) = [];
